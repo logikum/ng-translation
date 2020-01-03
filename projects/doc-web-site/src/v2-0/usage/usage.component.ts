c2d14607d0585c09808d4ef09bb17b9cf3d3a328
Mármarosi József <marmarosi@logikum.hu>
2020-01-03 20:30:18 +0100

    Extend documentation.

--- projects/doc-web-site/src/v2-0/usage/usage.component.ts
+++ projects/doc-web-site/src/v2-0/usage/usage.component.ts
@@ -1,11 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import _translation1 from 'raw-loader!./define/_translation-1.txt';
+import _placeholder from 'raw-loader!./define/_placeholder.txt';
 import _keys1 from 'raw-loader!./define/_keys-1.txt';
 import _translation2 from 'raw-loader!./define/_translation-2.txt';
 import _keys2 from 'raw-loader!./define/_keys-2.txt';
 import _translation3 from 'raw-loader!./define/_translation-3.txt';
 import _keys3 from 'raw-loader!./define/_keys-3.txt';
+
+import _directive1 from 'raw-loader!./structural/_directive-1.txt';
+import _directive2 from 'raw-loader!./structural/_directive-2.txt';
+import _directive3 from 'raw-loader!./structural/_directive-3.txt';
+import _param_1 from 'raw-loader!./structural/_param-1.txt';
+import _param_2 from 'raw-loader!./structural/_param-2.txt';
+import _html_1 from 'raw-loader!./structural/_html-1.txt';
+import _html_2 from 'raw-loader!./structural/_html-2.txt';
 
 import _pipe1 from 'raw-loader!./pipe/_pipe-1.txt';
 import _pipe2 from 'raw-loader!./pipe/_pipe-2.txt';
@@ -31,11 +40,20 @@ import _interpolation3 from 'raw-loader!./params/_interpolation-3.txt';
 export class UsageComponent {
 
   readonly translation1 = _translation1;
+  readonly placeholder = _placeholder;
   readonly keys1 = _keys1;
   readonly translation2 = _translation2;
   readonly keys2 = _keys2;
   readonly translation3 = _translation3;
   readonly keys3 = _keys3;
+
+  readonly directive1 = _directive1;
+  readonly directive2 = _directive2;
+  readonly directive3 = _directive3;
+  readonly param_1 = _param_1;
+  readonly param_2 = _param_2;
+  readonly html_1 = _html_1;
+  readonly html_2 = _html_2;
 
   readonly pipe1 = _pipe1;
   readonly pipe2 = _pipe2;
@@ -47,6 +65,7 @@ export class UsageComponent {
 
   readonly service1 = _service1;
   readonly service2 = _service2;
+
   readonly interpolation1 = _interpolation1;
   readonly interpolation2 = _interpolation2;
   readonly interpolation3 = _interpolation3;

751f0ed034c4c06f75e42ba966874bdc530b0d60
Mármarosi József <marmarosi@logikum.hu>
2020-01-06 21:59:52 +0100

    Update tests and documentation.

--- projects/doc-web-site/src/v3-0/usage/usage.component.ts
+++ projects/doc-web-site/src/v3-0/usage/usage.component.ts
@@ -8,13 +8,14 @@ import _keys2 from 'raw-loader!./define/_keys-2.txt';
 import _translation3 from 'raw-loader!./define/_translation-3.txt';
 import _keys3 from 'raw-loader!./define/_keys-3.txt';
 
-import _directive1 from 'raw-loader!./structural/_directive-1.txt';
-import _directive2 from 'raw-loader!./structural/_directive-2.txt';
-import _directive3 from 'raw-loader!./structural/_directive-3.txt';
-import _param_1 from 'raw-loader!./structural/_param-1.txt';
-import _param_2 from 'raw-loader!./structural/_param-2.txt';
-import _html_1 from 'raw-loader!./structural/_html-1.txt';
-import _html_2 from 'raw-loader!./structural/_html-2.txt';
+import _structural1 from 'raw-loader!./structural/_structural-1.txt';
+import _structural2 from 'raw-loader!./structural/_structural-2.txt';
+import _structural3 from 'raw-loader!./structural/_structural-3.txt';
+import _param_s1 from 'raw-loader!./structural/_param-s-1.txt';
+import _param_s2 from 'raw-loader!./structural/_param-s-2.txt';
+import _node from 'raw-loader!./structural/_node.txt';
+import _html_s1 from 'raw-loader!./structural/_html-s-1.txt';
+import _html_s2 from 'raw-loader!./structural/_html-s-2.txt';
 
 import _pipe1 from 'raw-loader!./pipe/_pipe-1.txt';
 import _pipe2 from 'raw-loader!./pipe/_pipe-2.txt';
@@ -23,6 +24,12 @@ import _param1 from 'raw-loader!./pipe/_param-1.txt';
 import _param2 from 'raw-loader!./pipe/_param-2.txt';
 import _html1 from 'raw-loader!./pipe/_html-1.txt';
 import _html2 from 'raw-loader!./pipe/_html-2.txt';
+
+import _attribute1 from 'raw-loader!./attribute/_attribute-1.txt';
+import _attribute2 from 'raw-loader!./attribute/_attribute-2.txt';
+import _attribute3 from 'raw-loader!./attribute/_attribute-3.txt';
+import _param_a1 from 'raw-loader!./attribute/_param-a-1.txt';
+import _param_a2 from 'raw-loader!./attribute/_param-a-2.txt';
 
 import _service1 from 'raw-loader!./service/_service-1.txt';
 import _service2 from 'raw-loader!./service/_service-2.txt';
@@ -47,13 +54,14 @@ export class UsageComponent {
   readonly translation3 = _translation3;
   readonly keys3 = _keys3;
 
-  readonly directive1 = _directive1;
-  readonly directive2 = _directive2;
-  readonly directive3 = _directive3;
-  readonly param_1 = _param_1;
-  readonly param_2 = _param_2;
-  readonly html_1 = _html_1;
-  readonly html_2 = _html_2;
+  readonly structural1 = _structural1;
+  readonly structural2 = _structural2;
+  readonly structural3 = _structural3;
+  readonly param_s1 = _param_s1;
+  readonly param_s2 = _param_s2;
+  readonly node = _node;
+  readonly html_s1 = _html_s1;
+  readonly html_s2 = _html_s2;
 
   readonly pipe1 = _pipe1;
   readonly pipe2 = _pipe2;
@@ -62,6 +70,12 @@ export class UsageComponent {
   readonly param2 = _param2;
   readonly html1 = _html1;
   readonly html2 = _html2;
+
+  readonly attribute1 = _attribute1;
+  readonly attribute2 = _attribute2;
+  readonly attribute3 = _attribute3;
+  readonly param_a1 = _param_a1;
+  readonly param_a2 = _param_a2;
 
   readonly service1 = _service1;
   readonly service2 = _service2;

5b9656c8a4cdf5fb0ec45d4fb09e824d68f74f75
Mármarosi József <marmarosi@logikum.hu>
2022-12-10 23:28:45 +0100

    Refactor loaders.

--- projects/ng-translation/src/lib/types.ts
+++ projects/ng-translation/src/lib/types.ts
@@ -4,5 +4,7 @@
 
 export type CurrencyValue = [ number, string ];
 export type LoaderType = 'json' | 'text' | 'blob' | 'arraybuffer' | 'inline';
+export type TextListKeys = string | Array<string> | object;
+
 export type HashMap<T = any> = { [key: string]: T };
 export type InlineLoaderMap = HashMap<( lang: string, name: string ) => Promise<object>>;
